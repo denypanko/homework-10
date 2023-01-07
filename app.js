41551b9905d5ed143e3c59926a5e6278ca18c896
Denys <denyspanasenko95@gmail.com>
2023-01-08 00:51:33 +0200

    add likes and scroll modal

--- app.js
+++ app.js
@@ -6,3 +6,57 @@ addToCartBtns.forEach((item) =>
     productsCountEl.textContent = +productsCountEl.textContent + 1;
   })
 );
+
+// modal
+
+let modal = document.querySelector(".modal");
+let moreDetailsBtns = document.querySelectorAll(".details-btn");
+let closeBtn = document.querySelector(".btn-close");
+
+moreDetailsBtns.forEach((item) => item.addEventListener("click", openModal));
+
+closeBtn.addEventListener("click", closeModal);
+
+modal.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    closeModal();
+  }
+});
+
+function openModal() {
+  modal.classList.add("show");
+  modal.classList.remove("hide");
+  // modal.style.display = "block";
+}
+
+function closeModal() {
+  modal.classList.add("hide");
+  modal.classList.remove("show");
+  //   modal.style.display = "none";
+}
+
+// modal scroll
+
+window.addEventListener("scroll", function () {
+  let scrollPercent =
+    (document.documentElement.scrollTop + document.body.scrollTop) /
+    (document.documentElement.scrollHeight -
+      document.documentElement.clientHeight);
+  if (scrollPercent > 0.5) {
+    modal.classList.add("show");
+  }
+});
+
+// likes
+
+let likes = document.querySelectorAll(".like");
+
+likes.forEach((item) => item.addEventListener("click", clickedLike));
+
+function clickedLike() {
+  if (this.classList.contains("clicked-like")) {
+    this.classList.remove("clicked-like");
+  } else {
+    this.classList.add("clicked-like");
+  }
+}
