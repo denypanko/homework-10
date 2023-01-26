af8b1eda154c3e99018423d940a45209eae4a983
Denys <denyspanasenko95@gmail.com>
2023-01-26 22:37:47 +0200

    add slider and product quantity

--- app.js
+++ app.js
@@ -54,9 +54,58 @@ let likes = document.querySelectorAll(".like");
 likes.forEach((item) => item.addEventListener("click", clickedLike));
 
 function clickedLike() {
-  if (this.classList.contains("clicked-like")) {
-    this.classList.remove("clicked-like");
-  } else {
-    this.classList.add("clicked-like");
-  }
+  // if (this.classList.contains("clicked-like")) {
+  //   this.classList.remove("clicked-like");
+  // } else {
+  //   this.classList.add("clicked-like");
+  // }
+  this.classList.toggle("clicked-like");
+}
+
+// slider
+$(".slider").slick({
+  autoplay: true,
+  dot: true,
+});
+
+const decrementBtns = document.querySelectorAll(".decrement-btn");
+const incrementBtns = document.querySelectorAll(".increment-btn");
+const quantityInputs = document.querySelectorAll(".quantity-input");
+
+function Counter(incrementBtn, decrementBtn, inputField) {
+  this.domRefs = {
+    incrementBtn,
+    decrementBtn,
+    inputField,
+  };
+
+  this.toggleButtonState = function () {
+    const count = this.domRefs.inputField.value;
+    this.domRefs.decrementBtn.disabled = count <= 1;
+    this.domRefs.incrementBtn.disabled = count >= 10;
+  };
+
+  this.increment = function () {
+    this.domRefs.inputField.value = +this.domRefs.inputField.value + 1;
+    this.toggleButtonState();
+  };
+
+  this.decrement = function () {
+    this.domRefs.inputField.value = +this.domRefs.inputField.value - 1;
+    this.toggleButtonState();
+  };
+
+  this.domRefs.incrementBtn.addEventListener(
+    "click",
+    this.increment.bind(this)
+  );
+  this.domRefs.decrementBtn.addEventListener(
+    "click",
+    this.decrement.bind(this)
+  );
+  this.toggleButtonState();
+}
+
+for (let i = 0; i < decrementBtns.length; i += 1) {
+  new Counter(incrementBtns[i], decrementBtns[i], quantityInputs[i]);
 }
